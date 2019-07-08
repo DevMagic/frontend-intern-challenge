885274b8fdd88891ebcc0b07e340036ac66c491b
isnack <isnack_yagami@hotmail.com>
2019-07-08 01:11:44 -0300

    add image url in javascript for url loader to work

---
+++ project/src/js/images.js
@@ -0,0 +1,5 @@
+import * as facebook from '../img/icon-facebook.png';
+import * as twitter from '../img/icon-twitter.png';
+import * as logo_chaordic from '../img/logo-chaordic.png';
+
+export { facebook, twitter, logo_chaordic };
