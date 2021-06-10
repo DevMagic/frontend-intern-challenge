b27b778c698cedeaddd16b34c2664faee61efd66
anderrf <anderson_rocha_farias@outlook.com>
2021-06-10 18:37:13 -0300

    JSON consuming for top 5 area

--- project/scripts/landing.js
+++ project/scripts/landing.js
@@ -87,7 +87,60 @@ document.querySelector('#shorten-form').addEventListener("submit", ev => {ev.pre
 //Verifies button condition on clicking
 document.querySelector('#btn-link').addEventListener("click", verifyButtonAction);
 
+//Add the top 5 urls to the page
+function listTopFive(tops)
+{
+    let list = document.querySelector('.top-link-list');
+    list.innerHTML = "";
+    for(let top of tops)
+    {
+        list.innerHTML += `
+            <div>
+                <p><a href="${top.url}">${top.shortUrl}</a></p>
+                <p>${top.hits}</p>
+            </div>
+        `;
+    }
+}
+
+//Selects the top 5 urls by hits
+function fillTopFive(allURLs)
+{
+    let top = [];
+    let qtTop = 0;
+    while(qtTop < 5 && allURLs.length >= 5)
+    {
+        for(let url of allURLs)
+        {
+            if(url === allURLs[0])
+            {
+                top.push(url);
+            }
+            else
+            {
+                if(url.hits > top[qtTop].hits)
+                {
+                    top[qtTop] = url;
+                }
+            }
+        }
+        allURLs.splice(allURLs.lastIndexOf(top[qtTop]), 1);
+        qtTop++;
+    }
+    //console.log(top);
+    listTopFive(top);
+}
+
+//Fetches url array
+function fetchUrls()
+{
+    fetch('../../Assets/urls.json')
+        .then(resp => {return resp.json()})
+        .then(data => {fillTopFive(data)});
+}
+
 //Functions activated on page loading
 window.addEventListener("load", function(){
     changeForm("shorten");
+    fetchUrls();
 });
